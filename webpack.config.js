e8c01505d253f9ad184848da58f38fab6d7df75e
Kayvine <kevin.delme@gmail.com>
2019-03-26 16:30:57 +0100

    commit step2

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+module.export = {
+  entry: {
+    app: './src/index.js'
+  },
+  devServer: {
+    hot: true,
+    compress: true
+  },
+  watch: true,
+  devtool: 'source-map',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
